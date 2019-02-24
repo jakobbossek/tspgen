1384332b5c54e6eee87130dd7b8f2b1b3a8b1211
Jakob Bossek <info@jakobbossek.de>
2019-02-24 12:04:33 +1030

    finish build() function

---
+++ man-roxygen/arg_coords.R
@@ -0,0 +1,2 @@
+#' @param coords [\code{matrix}]\cr
+#'   An n times 2 matrix of point coordinates in \eqn{[0, 1]^2}.
---
+++ man-roxygen/arg_pm.R
@@ -0,0 +1,3 @@
+#' @param pm [\code{numeric(1)}]\cr
+#'   Probability of node mutation. Note that each node is subject to mutation
+#'   independent of the the nodes.
