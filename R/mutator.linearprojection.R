8b31077ac3e8b565faa96902340f28cdb2940dbc
Jakob Bossek <info@jakobbossek.de>
2019-03-06 09:04:08 +0100

    improve docs of linear projection mutation

--- R/mutator.linearprojection.R
+++ R/mutator.linearprojection.R
@@ -3,14 +3,14 @@
 #'
 #' @description This is a generalization of the \code{\link{doAxisProjectionMutation}}.
 #' A subset \eqn{Q \subseteq P} (each point selected with independent
-#' probability \code{pm}) is selected. Next, a random intercept \eqn{a \in [0, 1]}
-#' is sampled. In a subsequent step a slope \eqn{m} is sampled uniformly at random from
-#' \eqn{[0, 3]} (if \eqn{a < 0.5}) and \eqn{[-3, 0]} (if \eqn{a \geq 0.5}). This heuristic
+#' probability \code{pm}) is selected. Next, a random intercept \eqn{\beta_0 \in [0, 1]}
+#' is sampled. In a subsequent step a slope \eqn{\beta_1} is sampled uniformly at random from
+#' \eqn{[0, 3]} (if \eqn{\beta_0 < 0.5}) and \eqn{[-3, 0]} (if \eqn{\beta_0 \geq 0.5}). This heuristic
 #' distinction of cases ensures that with high probability the resulting linear function
-#' \eqn{a + mx} runs inside the bounding-box \eqn{[0, 1]^2} at least partially. Finally, all
-#' points \eqn{p \in Q} are subject to an orthogonal projection on \eqn{a + mx}.
-#' Additionally, with probability \code{p.jitter}, Gaussian noise is added to the
-#' second coordinate of the projected points.
+#' \eqn{\beta_0 + \beta_1x} runs inside the bounding-box \eqn{[0, 1]^2} at least partially. Finally, all
+#' points \eqn{p \in Q} are modified by setting \eqn{p_2 = \beta_0 + \beta_1p_1}.
+#' Additionally, with probability \code{p.jitter}, Gaussian noise with mean zero
+#' and standard deviation \code{jitter.sd} is added to the second coordinate.
 #'
 #' @template arg_coords
 #' @template arg_pm
@@ -33,27 +33,14 @@ doLinearProjectionMutation = function(coords, pm = 0.1, p.jitter = 0, jitter.sd 
   if (n.mutants < 2L)
     return(coords)
 
-  # sample linear function
-  intercept = runif(1L)
+  linear = getRandomLinearFunction()
 
-  # we do not want, e.g., a positive slope if intercept is close to 1
-  slope = if (intercept < 0.5)
-    stats::runif(1L, min = 0, max = 3)
-  else
-    stats::runif(1L, min = -3, max = 0)
-
-  # helper function
-  linFun = function(x) {
-    intercept + slope * x
-  }
-
-  coords[to.mutate, 2L] = linFun(coords[to.mutate, 1L])
+  coords[to.mutate, 2L] = linear$linFun(coords[to.mutate, 1L])
 
   # jitter points vertically around projected line
   if (stats::runif(1L) < p.jitter & jitter.sd > 0) {
     coords[to.mutate, 2L] = coords[to.mutate, 2L] + stats::rnorm(n.mutants, sd = jitter.sd)
   }
 
-  #coords[to.mutate, ] = forceToBounds(coords[to.mutate, ])
   return(coords)
 }
